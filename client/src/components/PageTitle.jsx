a1a91272524cb29919852a861da98f34490dba15
Saurabh Ghiya <saurabhghiya@gmail.com>
2023-10-08 21:22:04 +0530

    admin page structure & UI done

---
+++ client/src/components/PageTitle.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function PageTitle({title}){
+
+    return (
+        <h1 className="text-xl uppercase">
+            {title}
+        </h1>
+    )
+}
--- client/src/pages/Admin/index.jsx
+++ client/src/pages/Admin/index.jsx
@@ -1,10 +1,20 @@
-
+import { Tabs } from 'antd';
+import PageTitle from '../../components/PageTitle';
+import MoviesList from './MoviesList';
 
 export default function Admin(){
+    
+    return (<>
+        <PageTitle title="Admin" />
+        <Tabs type="card" defaultActiveKey='1'>
 
-    return (
-        <div>
-            I am Admin
-        </div>
+            <Tabs.TabPane tab="Movies" key="1">
+                <MoviesList/>
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Theatres" key="2">
+                <h1>Theatre table</h1>
+            </Tabs.TabPane>
+        </Tabs>
+    </>
     )
 }
